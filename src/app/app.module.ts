f1af394d967bef3e7e7fa0ad5e31b6fb6e842bd4
Carl Eslabon <carleslabon@gmail.com>
2019-02-22 16:48:31 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {SuccessMessageComponent} from './success-message/success-message.component';
+import {WarningMessageComponent} from './warning-message/warning-message.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessMessageComponent,
+    WarningMessageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
